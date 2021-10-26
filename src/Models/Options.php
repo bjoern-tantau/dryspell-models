c247d5dc8bdc4c55bc09ced7bde6cf6b984707a5
Björn Tantau <bjoern@bjoern-tantau.de>
2021-10-26 20:51:24 +0200

    Adds more useful options.

--- src/Models/Options.php
+++ src/Models/Options.php
@@ -18,18 +18,26 @@ class Options
      *
      * @param string|null $type Fully qualified type of the property. Will usually not be set in the attribute.
      * @param bool $required Whether the property is required.
-     * @param type $default The properties default value. Use "now" for DateTime properties.
+     * @param type $default The properties default value.
+     *                      Use "now" for DateTime properties.
      * @param bool $nullable Whether the property is nullable. Will usually not be set in the attribute.
      * @param int|null $length The maximum length of the property value.
      * @param bool $generatedValue Whether the value of the property is generated.
      * @param bool $id Whether the property is used as an identifier.
      * @param bool $signed Whether the integer value of the property is signed.
-     * @param string|null $onUpdate Value to assign to the property on every update. Use "now" for DateTime properties.
+     * @param string|null $onUpdate Value to assign to the property on every update.
+     *                              Use "now" for DateTime properties.
+     *                              Use "restrict", "cascade", "set null", "set default" for references to other objects.
+     * @param string|null $onDelete Value to assign when a foreign object is deleted.
+     *                              Use "restrict", "cascade", "set null", "set default" for references to other objects.
+     * @param bool $searchable Create an index to search this property?
+     * @param bool $unique Is the property value unique?
      */
     public function __construct(
         public ?string $type = null, public bool $required = false, public $default = null,
         public bool $nullable = false, public ?int $length = null, public bool $generatedValue = false,
-        public bool $id = false, public bool $signed = true, public ?string $onUpdate = null
+        public bool $id = false, public bool $signed = true, public ?string $onUpdate = null,
+        public ?string $onDelete = null, public bool $searchable = false, public bool $unique = false
     )
     {
 
